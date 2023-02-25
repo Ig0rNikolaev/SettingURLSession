a587ac221fd5439fdb3f242e185285fba5e60c43
Igor Nikolaev <igor01131987@gmail.com>
2023-02-26 02:02:35 +0300

    Add marvel url

--- SettingURLSession.playground/Contents.swift
+++ SettingURLSession.playground/Contents.swift
@@ -1,11 +1,13 @@
 import Foundation
+import CryptoKit
 
 class CreatureURL {
 
-    func buildNbaURL() -> URL? {
-        let scheme = "https"
-        let host = "any-api.com"
-        let path = "/nba_com/nba_com/docs/API_Description"
+    func md5Hash(_ source: String) -> String {
+        return Insecure.MD5.hash(data: source.data(using: .utf8)!).map { String(format: "%02hhx", $0) }.joined()
+    }
+
+    func buildURL(scheme: String, host: String, path: String) -> URL? {
 
         var components: URLComponents = {
             var components = URLComponents()
@@ -18,9 +20,29 @@ class CreatureURL {
     }
 }
 
-class CreatureMarvelURL: CreatureURL {
-    override func buildNbaURL() -> URL? {
-        <#code#>
+class CreatureURLMarvel: CreatureURL {
+
+    override func buildURL(scheme: String, host: String, path: String) -> URL? {
+
+        let publicKey = "8ff288aa4d9bd9b25d051aea9585e041"
+        let privateKey = "745eeeff855e804c98b080fd6570b1b2b8553550"
+        let ts = NSDate().timeIntervalSince1970.description
+        let hashString = ts + privateKey + publicKey
+        let hash = md5Hash(hashString)
+        let queryItem = [
+            URLQueryItem(name: "ts", value: ts),
+            URLQueryItem(name: "apikey", value: publicKey),
+            URLQueryItem(name: "hash", value: hash)
+        ]
+        var components: URLComponents = {
+            var components = URLComponents()
+            components.scheme = scheme
+            components.host = host
+            components.path = path
+            components.queryItems = queryItem
+            return components
+        }()
+        return components.url
     }
 }
 
@@ -32,22 +54,21 @@ final class CreatureURLRequest {
             if error != nil {
                 print("Ошибка при запросе: \(String(describing: error?.localizedDescription))")
             }
-
             guard let response = response as? HTTPURLResponse else { return }
 
             switch response.statusCode {
             case 100...103:
-                print("\(response.statusCode)")
+                print("Информационный код: \(response.statusCode)")
             case 200...299:
                 guard let data = data else { return }
                 let dataAsString = String(data: data, encoding: .utf8)
                 print("Код ответа от сервера:\n \(response)\n Данные, пришедшие с сервера:\n \(String(describing: dataAsString))")
             case 300...399:
-                print("\(response.statusCode)")
+                print("Сообщение о перенаправлении: \(response.statusCode)")
             case 400...499:
-                print("\(response.statusCode)")
+                print("Клиентская ошибка: \(response.statusCode)")
             case 500...599:
-                print("\(response.statusCode)")
+                print("Серверная ошибка: \(response.statusCode)")
             default:
                 break
             }
@@ -55,11 +76,12 @@ final class CreatureURLRequest {
     }
 }
 
-var urlNba = CreatureURL()
-var request = CreatureURLRequest()
+let requestURL = CreatureURLRequest()
 
-request.getData(urlRequest: urlNba.buildNbaURL())
+let urlNba = CreatureURL()
+let nba = urlNba.buildURL(scheme: "https", host: "any-api.com", path: "/nba_com/nba_com/docs/API_Description")
+requestURL.getData(urlRequest: nba)
 
-
-
-
+let urlMarvel = CreatureURLMarvel()
+let marvel = urlMarvel.buildURL(scheme: "https", host: "gateway.marvel.com", path: "/v1/public/comics")
+requestURL.getData(urlRequest: marvel)
